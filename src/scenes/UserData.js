8da510963c53cd0c539cb8c092b37b2467afb6f5
Damien Gaillard <damien.gaillard@vossloh.com>
2021-06-24 15:56:09 +0200

    example rewritten as a functional component, making use of react hooks

--- src/scenes/UserData.js
+++ src/scenes/UserData.js
@@ -1,157 +1,154 @@
 /* This is where the uibuilder action happens */
-import React, { Component } from 'react';
+import React, {useState, useEffect} from 'react';
 //import ReactDOM from 'react-dom';
 //import { findDOMNode } from 'react-dom';
 
-import uibuilder from 'node-red-contrib-uibuilder/front-end/src/uibuilderfe'
+import uibuilder from 'node-red-contrib-uibuilder/front-end/src/uibuilderfe';
 
-class UserData extends Component{
-	constructor(props){
-		super(props)
+function UserData(props) {
 
-        /** **REQUIRED** Start uibuilder comms with Node-RED @since v2.0.0-dev3
-         * Pass the namespace and ioPath variables if hosting page is not in the instance root folder
-         * e.g. If you get continual `uibuilderfe:ioSetup: SOCKET CONNECT ERROR` error messages.
-         * e.g. uibuilder.start('/nr/uib', '/nr/uibuilder/vendor/socket.io') // change to use your paths/names
-         */
-        uibuilder.start();
-
-		this.state = {
-            // Example of retrieving data from uibuilder
-            feVersion   : uibuilder.get('version'),
-
-            socketConnectedState : false,
-            serverTimeOffset     : '[unknown]',
-
-            msgRecvd    : '[Nothing]',
-            msgsReceived: 0,
-            msgCtrl     : '[Nothing]',
-            msgsControl : 0,
-
-            msgSent     : '[Nothing]',
-            msgsSent    : 0,
-            msgCtrlSent : '[Nothing]',
-            msgsCtrlSent: 0,
-        }
-
-        /** You can use the following to help trace how messages flow back and forth.
-         * You can then amend this processing to suite your requirements.
-         */
-
-        //#region ---- Trace Received Messages ---- //
-        // If msg changes - msg is updated when a standard msg is received from Node-RED over Socket.IO
-        // newVal relates to the attribute being listened to.
-		uibuilder.onChange('msg', (newVal) => {
-
-			this.setState({ 'msgRecvd': newVal });
-
-			console.info('[uibuilder.onChange] msg received from Node-RED server:', newVal);
-		})
-
-        // As we receive new messages, we get an updated count as well
-		uibuilder.onChange('msgsReceived',(newVal) =>{
-            console.info('[uibuilder.onChange] Updated count of received msgs:', newVal);
-
-			this.setState({ 'msgsReceived': newVal });
-		})
-
-        // If we receive a control message from Node-RED, we can get the new data here - we pass it to a Vue variable
-        uibuilder.onChange('ctrlMsg', (newVal) => {
-            console.info('[uibuilder.onChange:ctrlMsg] CONTROL msg received from Node-RED server:', newVal);
-
-            this.setState({ 'msgCtrl': newVal });
-        })
-        // Updated count of control messages received
-        uibuilder.onChange('msgsCtrl', (newVal) => {
-            console.info('[uibuilder.onChange:msgsCtrl] Updated count of received CONTROL msgs:', newVal);
-
-            this.setState({ 'msgsControl': newVal });
-        })
-        //#endregion ---- End of Trace Received Messages ---- //
-
-        //#region ---- Trace Sent Messages ---- //
-        // You probably only need these to help you understand the order of processing //
-        // If a message is sent back to Node-RED, we can grab a copy here if we want to
-        uibuilder.onChange('sentMsg', (newVal) => {
-            console.info('[uibuilder.onChange:sentMsg] msg sent to Node-RED server:', newVal);
-
-            this.setState({ 'msgSent': newVal });
-        })
-        // Updated count of sent messages
-        uibuilder.onChange('msgsSent', (newVal) => {
-            console.info('[uibuilder.onChange:msgsSent] Updated count of msgs sent:', newVal);
-
-            this.setState({ 'msgsSent': newVal });
-        })
-
-        // If we send a control message to Node-RED, we can get a copy of it here
-        uibuilder.onChange('sentCtrlMsg', (newVal) => {
-            console.info('[uibuilder.onChange:sentCtrlMsg] Control message sent to Node-RED server:', newVal);
-
-            this.setState({ 'msgCtrlSent': newVal });
-        })
-        // And we can get an updated count
-        uibuilder.onChange('msgsSentCtrl', (newVal) => {
-            console.info('[uibuilder.onChange:msgsSentCtrl] Updated count of CONTROL msgs sent:', newVal);
-
-            this.setState({ 'msgsCtrlSent': newVal });
-        })
-        //#endregion ---- End of Trace Sent Messages ---- //
-
-        // If Socket.IO connects/disconnects, we get true/false here
-        uibuilder.onChange('ioConnected', (newVal) => {
-            console.info('[uibuilder.onChange:ioConnected] Socket.IO Connection Status Changed to:', newVal)
-
-            this.setState({ 'socketConnectedState': newVal })
-        })
-        // If Server Time Offset changes
-        uibuilder.onChange('serverTimeOffset', (newVal) =>{
-            console.info('[uibuilder.onChange:serverTimeOffset] Offset of time between the browser and the server has changed to:', newVal)
-
-            this.setState({ 'serverTimeOffset': newVal })
-        })
-
-        	//Manually send a message back to Node-RED after 2 seconds
-        	window.setTimeout(function(){
-        	    console.info('Sending a message back to Node-RED-after2sdelay')
-        	    uibuilder.send({'topic':'uibuilderfe','payload':'I am a message sent from the uibuilder front end'})
-        	},2000)
-	}
+    // Example of retrieving data from uibuilder
+    const [feVersion, setFeVersion] = useState(uibuilder.get('version'));
+    const [socketConnectedState, setSocketConnectedState] = useState(false);
+    const [serverTimeOffset, setServerTimeOffset] = useState('[unknown]');
+    const [msgRecvd, setMsgRecvd] = useState('[Nothing]');
+    const [msgsReceived, setMsgsReceived] = useState(0);
+    const [msgCtrl, setMsgCtrl] = useState('[Nothing]');
+    const [msgsControl, setMsgsControl] = useState(0);
+    const [msgSent, setMsgSent] = useState('[Nothing]');
+    const [msgsSent, setMsgsSent] = useState(0);
+    const [msgCtrlSent, setMsgCtrlSent] = useState('[Nothing]');
+    const [msgsCtrlSent, setMsgsCtrlSent] = useState(0);
 
 
-	render(){
-		return(
+    useEffect(
+        () => {
+            /** **REQUIRED** Start uibuilder comms with Node-RED @since v2.0.0-dev3
+             * Pass the namespace and ioPath variables if hosting page is not in the instance root folder
+             * e.g. If you get continual `uibuilderfe:ioSetup: SOCKET CONNECT ERROR` error messages.
+             * e.g. uibuilder.start('/nr/uib', '/nr/uibuilder/vendor/socket.io') // change to use your paths/names
+             */
+            uibuilder.start();
 
-			<div ref="root" style={{height:"50vh"}}>
-                <hr></hr>
-                <div className="d1">
-                    <div>Last msg Received:</div>
-                    <pre><code>{JSON.stringify(this.state.msgRecvd, null, 2)}</code></pre>
-                    <div># Msgs Received: {this.state.msgsReceived}</div>
-                </div>
 
-                <div className="d1">
-                    <div>last Ctl Msg Received:</div>
-                    <pre><code>{JSON.stringify(this.state.msgCtrl, null, 2)}</code></pre>
-                    <div># Control Msgs Received: {this.state.msgsControl}</div>
-                </div>
+            /** You can use the following to help trace how messages flow back and forth.
+             * You can then amend this processing to suite your requirements.
+             */
 
-                <div className="d1">
-                    <div>last Msg Sent</div>
-                    <pre><code>{JSON.stringify(this.state.msgSent, null, 2)}</code></pre>
-                    <div># msgs Sent: {this.state.msgsSent}</div>
-                </div>
+            //#region ---- Trace Received Messages ---- //
+            // If msg changes - msg is updated when a standard msg is received from Node-RED over Socket.IO
+            // newVal relates to the attribute being listened to.
+            uibuilder.onChange('msg', (newVal) => {
+                console.info('[uibuilder.onChange] msg received from Node-RED server:', newVal);
+                setMsgRecvd(newVal);
+            });
 
-                <div className="d1">
-                    <div>Socket Connected?: {this.state.socketConnectedState}</div>
-                    <div>uibuilderfe Version: {this.state.feVersion}</div>
-                    <div>Server Time Offset from browser: {this.state.serverTimeOffset}</div>
-                </div>
+            // As we receive new messages, we get an updated count as well
+            uibuilder.onChange('msgsReceived', (newVal) => {
+                console.info('[uibuilder.onChange] Updated count of received msgs:', newVal);
+                setMsgsReceived(newVal);
+            });
 
-			</div>
-		);
+            // If we receive a control message from Node-RED, we can get the new data here - we pass it to a Vue variable
+            uibuilder.onChange('ctrlMsg', (newVal) => {
+                console.info('[uibuilder.onChange:ctrlMsg] CONTROL msg received from Node-RED server:', newVal);
+                setMsgCtrl(newVal);
+            });
 
-	}
+            // Updated count of control messages received
+            uibuilder.onChange('msgsCtrl', (newVal) => {
+                console.info('[uibuilder.onChange:msgsCtrl] Updated count of received CONTROL msgs:', newVal);
+                setMsgsControl(newVal);
+            });
+
+            //#endregion ---- End of Trace Received Messages ---- //
+
+            //#region ---- Trace Sent Messages ---- //
+            // You probably only need these to help you understand the order of processing //
+            // If a message is sent back to Node-RED, we can grab a copy here if we want to
+            uibuilder.onChange('sentMsg', (newVal) => {
+                console.info('[uibuilder.onChange:sentMsg] msg sent to Node-RED server:', newVal);
+                setMsgSent(newVal);
+            });
+
+            // Updated count of sent messages
+            uibuilder.onChange('msgsSent', (newVal) => {
+                console.info('[uibuilder.onChange:msgsSent] Updated count of msgs sent:', newVal);
+                setMsgsSent(newVal);
+            });
+
+            // If we send a control message to Node-RED, we can get a copy of it here
+            uibuilder.onChange('sentCtrlMsg', (newVal) => {
+                console.info('[uibuilder.onChange:sentCtrlMsg] Control message sent to Node-RED server:', newVal);
+                setMsgCtrlSent(newVal);
+            });
+
+            // And we can get an updated count
+            uibuilder.onChange('msgsSentCtrl', (newVal) => {
+                console.info('[uibuilder.onChange:msgsSentCtrl] Updated count of CONTROL msgs sent:', newVal);
+                setMsgsCtrlSent(newVal);
+            });
+
+            //#endregion ---- End of Trace Sent Messages ---- //
+
+            // If Socket.IO connects/disconnects, we get true/false here
+            uibuilder.onChange('ioConnected', (newVal) => {
+                console.info('[uibuilder.onChange:ioConnected] Socket.IO Connection Status Changed to:', newVal)
+                setSocketConnectedState(newVal);
+            });
+
+            // If Server Time Offset changes
+            uibuilder.onChange('serverTimeOffset', (newVal) => {
+                console.info('[uibuilder.onChange:serverTimeOffset] Offset of time between the browser and the server has changed to:', newVal)
+                setServerTimeOffset(newVal);
+            });
+
+            //Manually send a message back to Node-RED after 2 seconds
+            window.setTimeout(function () {
+                console.info('Sending a message back to Node-RED-after2sdelay')
+                uibuilder.send({'topic': 'uibuilderfe', 'payload': 'I am a message sent from the uibuilder front end'})
+            }, 2000);
+
+
+            return () => {
+                uibuilder.clearEventListeners();
+            };
+
+        },
+        []
+    );
+
+
+    return (
+
+        <div style={{height: "50vh"}}>
+            <hr></hr>
+            <div className="d1">
+                <div>Last msg Received:</div>
+                <pre><code>{JSON.stringify(msgRecvd, null, 2)}</code></pre>
+                <div># Msgs Received: {msgsReceived}</div>
+            </div>
+
+            <div className="d1">
+                <div>last Ctl Msg Received:</div>
+                <pre><code>{JSON.stringify(msgCtrl, null, 2)}</code></pre>
+                <div># Control Msgs Received: {msgsControl}</div>
+            </div>
+
+            <div className="d1">
+                <div>last Msg Sent</div>
+                <pre><code>{JSON.stringify(msgSent, null, 2)}</code></pre>
+                <div># msgs Sent: {msgsSent}</div>
+            </div>
+
+            <div className="d1">
+                <div>Socket Connected?: {socketConnectedState}</div>
+                <div>uibuilderfe Version: {feVersion}</div>
+                <div>Server Time Offset from browser: {serverTimeOffset}</div>
+            </div>
+
+        </div>
+    );
 
 }
 
